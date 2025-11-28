9c6bfd08f85d27aef3b3969f8b48bdcc3662167c
Medion M Narcisse <medionnarcisse68@gmail.com>
2025-11-28 01:40:30 +0100

    updates
    Removed base configuration and Proxmox provider settings.

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,20 +1,3 @@
-# Configuration de base
-terraform {
-  required_providers {
-    proxmox = {
-      source  = "bpg/proxmox"
-      version = "~> 0.82.0"
-    }
-  }
-}
-
-# Connexion à Proxmox
-provider "proxmox" {
-  endpoint  = var.proxmox_endpoint    # URL de Proxmox
-  api_token = var.proxmox_api_token   # Token secret
-  insecure  = true
-}
-
 # Liste de mes VMs
 locals {
   mes_vms = {
