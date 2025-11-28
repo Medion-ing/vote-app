d7ba07900fb480f20fbeb6d98854eb7e15f59cea
Medion M Narcisse <medionnarcisse68@gmail.com>
2025-11-28 01:41:48 +0100

    update provider configuration
    Updated Proxmox provider configuration to use variables for endpoint and API token.

--- infra/terraform/providers.tf
+++ infra/terraform/providers.tf
@@ -1,15 +1,16 @@
+# Configuration de base
 terraform {
   required_providers {
     proxmox = {
       source  = "bpg/proxmox"
-      version = "~> 0.82.0" # dernière version stable
+      version = "~> 0.82.0"
     }
   }
-  required_version = ">= 1.0.0"
 }
 
+# Connexion à Proxmox
 provider "proxmox" {
-  endpoint  = "https://192.168.1.42:8006/api2/json"
-  api_token = "terraform-prov@pam!terraform-token=7bf8551d-e898-41ea-9d05-d9ec1b838f13"
+  endpoint  = var.proxmox_endpoint    # URL de Proxmox
+  api_token = var.proxmox_api_token   # Token secret
   insecure  = true
 }
